f10ba0e1cc3385af629f0554135035aeaa7ebc89
Georgy Medvedsky <georgymedvedsky@gmail.com>
2024-06-06 16:08:56 +0300

    feat: add modal onClose func / add ingredient details in modal

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -13,80 +13,39 @@ import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import React from 'react';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 
-// Функция для проверки аутентификации пользователя
-const isAuthenticated = () => {};
+const App = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
 
-type ProtectedRouteProps = {
-  children: React.ReactElement;
+  const backgroundLocation = location.state?.backgroundLocation;
+
+  const handleClose = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes location={backgroundLocation || location}>
+        <Route path='*' element={<NotFound404 />} />
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/ingredients/:id' element={<ConstructorPage />} />
+      </Routes>
+
+      <Routes>
+        <Route
+          path='/ingredients/:id'
+          element={
+            <Modal title='Детали ингредиента' onClose={handleClose}>
+              <IngredientDetails />
+            </Modal>
+          }
+        />
+      </Routes>
+    </div>
+  );
 };
-
-// Компонент высшего порядка для защиты роутов
-// const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
-//   if (!isAuthenticated()) {
-//     return <Navigate to='/login' />;
-//   }
-//   return children;
-// };
-
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <Routes>
-      <Route path='*' element={<NotFound404 />} />
-      <Route path='/' element={<ConstructorPage />} />
-      <Route path='/feed' element={<Feed />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/forgot-password' element={<ForgotPassword />} />
-      <Route path='/reset-password' element={<ResetPassword />} />
-      <Route path='/profile' element={<Profile />} />
-      <Route path='/profile/orders' element={<ProfileOrders />} />
-    </Routes>
-    <Routes>
-      <Route
-        path='/feed/:number'
-        element={
-          <Modal
-            title='Информация о заказе'
-            onClose={() => {
-              console.log(1);
-            }}
-          >
-            <OrderInfo />
-          </Modal>
-        }
-      />
-      <Route
-        path='/ingredients/:id'
-        element={
-          <Modal
-            title='Информация о заказе'
-            onClose={() => {
-              console.log(1);
-            }}
-          >
-            <IngredientDetails />
-          </Modal>
-        }
-      />
-      <Route
-        path='/profile/orders/:number'
-        element={
-          <Modal
-            title='Информация о заказе'
-            onClose={() => {
-              console.log(1);
-            }}
-          >
-            <OrderInfo />
-          </Modal>
-        }
-      />
-    </Routes>
-  </div>
-);
 
 export default App;
